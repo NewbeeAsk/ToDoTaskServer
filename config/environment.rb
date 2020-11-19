118ed1c796a66f1fffa2cfb81149cffbd2ed1d2d
Roman Nosadchuk <romannosadchuk@gmail.com>
2020-11-19 13:06:19 +0200

    API3

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { :host => 'to-do-task-server.herokuapp.com'}
